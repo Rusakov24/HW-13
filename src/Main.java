f434370dd40a2e5b2952c7ea86943d2790a23c73
Rusakov24 <lovewillkillme@vk.com>
2024-07-29 20:42:59 +0300

    Конец работы

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
+        Author tolstoy = new Author("Лев", "Толстой");
 
+        Book childhood = new Book("Детство", tolstoy, 1851);
+        System.out.println(childhood);
+        System.out.println(childhood.getNameOfAuthor().getFirstName() + " " + childhood.getNameOfAuthor().getSecondName());
+        System.out.println(childhood.getBookTitle() +" " + childhood.getPublicationYear());
+        childhood.setPublicationYear(1851);
+        System.out.println(childhood.getPublicationYear());
+
+        Author pushkin = new Author("Лев", "Толстой");
+        Book onegin = new Book("Детство", pushkin, 1852);
+        System.out.println(onegin.getNameOfAuthor().getFirstName() + " " + onegin.getNameOfAuthor().getSecondName());
+        System.out.println(onegin.getBookTitle() + " " + onegin.getPublicationYear());
+
+        System.out.println("123");
+        System.out.println(childhood.getPublicationYear()==onegin.getPublicationYear());
+        System.out.println(childhood.equals((onegin)));
+        System.out.println(tolstoy.equals(pushkin));
     }
 }
