f06df44104a9f0ca0e64d23095bfec5ffa0c7b8f
Rusakov24 <lovewillkillme@vk.com>
2024-07-29 20:47:49 +0300

    Конец работы

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,15 @@ public class Main {
     public static void main(String[] args) {
         Author tolstoy = new Author("Лев", "Толстой");
 
-        Book childhood = new Book("Детство", tolstoy, 1851);
+        Book childhood = new Book("Война и мир", tolstoy, 1869);
         System.out.println(childhood);
         System.out.println(childhood.getNameOfAuthor().getFirstName() + " " + childhood.getNameOfAuthor().getSecondName());
         System.out.println(childhood.getBookTitle() +" " + childhood.getPublicationYear());
-        childhood.setPublicationYear(1851);
+        childhood.setPublicationYear(1869);
         System.out.println(childhood.getPublicationYear());
 
         Author pushkin = new Author("Лев", "Толстой");
-        Book onegin = new Book("Детство", pushkin, 1852);
+        Book onegin = new Book("Война и мир", pushkin, 1868);
         System.out.println(onegin.getNameOfAuthor().getFirstName() + " " + onegin.getNameOfAuthor().getSecondName());
         System.out.println(onegin.getBookTitle() + " " + onegin.getPublicationYear());
 
