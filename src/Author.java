d4b53c1e651857d224533b0a2cee3ab4f22b50e7
Rusakov24 <lovewillkillme@vk.com>
2025-02-18 16:12:09 +0300

    Исправление ошибки

--- src/Author.java
+++ src/Author.java
@@ -1,30 +1,39 @@
+import java.util.Objects;
+
 public class Author {
     private String firstName;
     private String secondName;
-    public Author (String firstName, String secondName) {
+
+    public Author(String firstName, String secondName) {
         this.firstName = firstName;
         this.secondName = secondName;
     }
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return  true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Author author = (Author) o;
-        return firstName.equals((author.firstName)) && secondName.equals(author.secondName);
 
-    }
-    @Override
-    public int hashCode() {
-        return java.util.Objects.hash(secondName);
-    }
     public String getFirstName() {
         return firstName;
     }
+
     public String getSecondName() {
         return secondName;
     }
+
     @Override
-    public String toString(){
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return firstName.equals(author.firstName) && secondName.equals(author.secondName);
+    }
+
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, secondName);
+    }
+
+
+    @Override
+    public String toString() {
         return this.getFirstName() + " " + this.getSecondName();
     }
 }
